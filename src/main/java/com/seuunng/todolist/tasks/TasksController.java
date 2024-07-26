da6c00e7b90fa119d8b2d4958de70445df58a31f
seuunng <155802305+seuunng@users.noreply.github.com>
2024-07-26 22:56:21 +0900

    메모 조회기능, 추가기능 추가

--- src/main/java/com/seuunng/todolist/tasks/TasksController.java
+++ src/main/java/com/seuunng/todolist/tasks/TasksController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,11 @@ public class TasksController {
 		List<TasksEntity> tasks = tasksRepository.findAll();
 		System.out.println(tasks);
 		return tasks;
+	}
+
+	@PostMapping("/task")
+	public TasksEntity addTask(@RequestBody TasksEntity newTask) {
+		TasksEntity task = tasksRepository.save(newTask);
+		return task;
 	}
 }
