55a4906e0bbe3e7c033d16d97173c745d6e78260
seuunng <155802305+seuunng@users.noreply.github.com>
2024-07-29 18:20:56 +0900

    리스트 조회 추가 기능 추가

--- src/main/java/com/seuunng/todolist/users/UsersRepository.java
+++ src/main/java/com/seuunng/todolist/users/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.seuunng.todolist.users;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<UsersEntity, Long>{
-
+	Optional<UsersEntity> findById(String id);
 }
